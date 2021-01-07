e6b2cf18bc9becc4317739a6a91c7b9350254f33
Diego <dibarra@dal.ca>
2021-01-07 11:31:07 -0400

    1st commit

---
+++ make_phytoplankton_plot.R
@@ -0,0 +1,11 @@
+
+library(ggplot2)
+
+data <- read.csv("data/phytoplankton_data.csv")
+
+ggplot(data, aes(x=month, y=phytoplankton)) + 
+  geom_line(colour = "red", size=2) + 
+  labs(title="Phytoplankton concentration in Nova Scotia",
+       subtitle="units: milimoles/m3",
+       caption="Source: NOAA, ERDDAP") +
+  theme(text = element_text(size=15))
